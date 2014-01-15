e46e8cad4caecaa3e4bee3b947d4e8b1da1b5bc6
bryancr89 <bazofeifa89@gmail.com>
2014-01-15 15:33:53 -0600

    Ready the RWD challenge

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+	var $nav = $('#main-header nav'),
+		$select = $('<select id="dropdown-menu" />');;
+
+	// Create default option 'Go to...'
+	$('<option />', {
+		'selected': 'selected',
+		'value'   : '',
+		'text'    : 'Go to...'
+	}).appendTo($select);
+
+	// Populate dropdown with menu items
+	$nav.find('a').each(function() {
+		var el = $(this);
+		$('<option />', {
+			'value'   : el.attr('href'),
+			'text'    : el.text()
+		}).appendTo($select);
+	});
+
+	$select.change(function() {
+		console.log($(this).find('option:selected').val());
+		window.location = $(this).find('option:selected').val();
+	});
+
+	$nav.append($select);
+});
